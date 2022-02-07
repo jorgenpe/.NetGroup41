988749af9f1a41307ab6a5f270e0c8a956aa3dde
jorgenpe <82228083+jorgenpe@users.noreply.github.com>
2022-02-07 14:55:20 +0100

    Add project files.

---
+++ ConsoleAppCSharpExercis/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleAppCSharpExercis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool a = true;
+            AgeForBeer(a);
+            
+            static void AgeForBeer(bool a) {
+
+                
+                do
+                {
+                    a = false;
+                    Console.Write("Ange din ålder: ");
+                    int b = Convert.ToInt32(Console.ReadLine());
+
+                    if (b > 17)
+                    {
+                        Console.WriteLine("Du kan drika en öl");
+                        a = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du kan få en cola.");
+                    }
+                    
+               
+                } while (a);
+            }
+        }
+    }
+}
